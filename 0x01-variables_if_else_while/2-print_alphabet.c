a5e81e3625196a32ca6dd821f513535179b16366
nidal1 <touhami9@gmail.com>
2023-03-17 14:10:49 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all alphabets in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = 97; i < 122; i++)
+	{
+		a = (char) i;
+		putchar(a);
+	}
+	putchar("\n");
+	return (0);
+}
