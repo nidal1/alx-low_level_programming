5ba66f85e81406e5ee9958a24310509652ad13b6
nidal1 <touhami9@gmail.com>
2023-03-27 17:26:18 -0700

    Add 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: pointer to string
+ *
+ * Return: len
+ */
+void rev_string(char *s)
+{
+	int i, len;
+	char swap;
+
+	if (*s != '\0')
+	{
+		len = 0;
+		while (*(s + len) != '\0')
+		{
+			len++;
+		}
+		i = 0;
+		len = len - 1;
+		while (i <= len)
+		{
+			swap = *(s + i);     /* swap = first */
+			*(s + i) = *(s + len); /* first = last */
+			*(s + len) = swap; /* last = swap (first) */
+			i++;
+			len--;
+		}
+	}
+}
