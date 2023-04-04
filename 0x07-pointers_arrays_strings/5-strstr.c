141a882c67065994550344971d0f2a09e39b88c7
nidal1 <touhami9@gmail.com>
2023-04-04 01:20:01 +0000

    Add _strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,10 +2,8 @@
 
 /**
  * _strstr - locates a substring
- * @haystack: Pointer string
- * 
- * @needle: Pointer char
- * 
+ * @haystack: string to be searched
+ * @needle: substring to be found
  * Return: pointer to substring or NULL If not found
  */
 char *_strstr(char *haystack, char *needle)
