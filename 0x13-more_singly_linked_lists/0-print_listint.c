4a73e529e93ef66431fbfb8d92f4b35924f5b7ab
Mohamed Nidal <touhami9@gmail.com>
2023-04-30 21:44:56 +0100

    remove if statement

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,21 +2,16 @@
 
 /**
  * struct print_listint - print integer of each node in the list
- * @h: struct listint_t
+ * @h: pointer to head struct listint_t
  *
  * Return: long unsigned integer counter
  */
 size_t print_listint(const listint_t *h)
 {
     size_t count = 0;
-    if (h == NULL)
-    {
-        return count;
-    }
-
     listint_t *nodePtr = h;
     while (nodePtr){
-        _putchar(nodePtr->n);
+        rpintf("%d\n", nodePtr->n);
         count++;
         nodePtr = nodePtr->next;
     }
