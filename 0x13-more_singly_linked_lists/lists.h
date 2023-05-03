b47f44b86ad08c298bebf3b0f4aef427bc6d9aee
Mohamed Nidal <touhami9@gmail.com>
2023-05-03 01:27:24 +0100

    fix new line

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stddef.h>
 /**
  * struct listint_s - singly linked list
@@ -29,3 +32,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+
+#endif /*LISTS_H*/
